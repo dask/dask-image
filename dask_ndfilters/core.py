1d3d7184ee0561f680001e18597d30bdfcf8b025
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-04-27 15:05:42 -0400

    Expose rank and percentile filters at top

--- dask_ndfilters/core.py
+++ dask_ndfilters/core.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from dask_ndfilters.order import minimum_filter, median_filter, maximum_filter
+from dask_ndfilters.order import (
+    minimum_filter,
+    median_filter,
+    maximum_filter,
+    rank_filter,
+    percentile_filter,
+)
