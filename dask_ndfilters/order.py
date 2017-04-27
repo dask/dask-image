e2aee75ef10b8375f767e2f55b59fc13c3927188
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-04-27 11:16:02 -0400

    Customize docstring of wrapping function

--- dask_ndfilters/order.py
+++ dask_ndfilters/order.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-import functools
+import inspect
 import numbers
+import re
 
 import numpy
 import scipy.ndimage.filters
@@ -93,8 +94,45 @@ def _get_normed_args(ndim, size=None, footprint=None, origin=0):
     return footprint, origin, depth, boundary
 
 
+def _get_docstring(func):
+    # Drop the output parameter from the docstring.
+    split_doc_params = lambda s: \
+        re.subn("(    [A-Za-z]+ : )", "\0\\1", s)[0].split("\0")
+    drop_doc_param = lambda s: not s.startswith("    output : ")
+    cleaned_docstring = "".join([
+        l for l in split_doc_params(func.__doc__) if drop_doc_param(l)
+    ])
+
+    docstring = """
+    Wrapped copy of "{mod_name}.{func_name}"
+
+
+    Excludes the output parameter as it would not work Dask arrays.
+
+
+    Original docstring:
+
+    {doc}
+    """.format(
+        mod_name=inspect.getmodule(func).__name__,
+        func_name=func.__name__,
+        doc=cleaned_docstring,
+    )
+
+    return docstring
+
+
+def _update_wrapper(func):
+    def _updater(wrapper):
+        wrapper.__name__ = func.__name__
+        wrapper.__doc__ = _get_docstring(func)
+        return wrapper
+
+    return _updater
+
+
 def _ordering_filter_wrapper(func):
-    @functools.wraps(func)
+    @_update_wrapper(func)
     def _wrapped_ordering_filter(input,
                                  size=None,
                                  footprint=None,
