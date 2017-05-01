c917acc71f976ad54e058122513c27f2c5343c96
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-01 15:38:48 -0400

    Add a utility function for depth and boundary
    Simply cleans up the depth and boundary. Also works around a bug in some
    versions of Dask.

--- dask_ndmorph/_utils.py
+++ dask_ndmorph/_utils.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 
 
+import collections
 import inspect
+import numbers
 import re
+
+
+try:
+    from itertools import imap, izip
+except ImportError:
+    imap, izip = map, zip
+
+try:
+    irange = xrange
+except NameError:
+    irange = range
+
+try:
+    unicode
+except NameError:
+    unicode = str
+
+strlike = (bytes, unicode)
 
 
 def _get_docstring(func):
@@ -40,3 +60,53 @@ def _update_wrapper(func):
         return wrapper
 
     return _updater
+
+
+def _get_depth_boundary(ndim, depth, boundary=None):
+    if not isinstance(ndim, numbers.Integral):
+        raise TypeError("Expected integer value for `ndim`.")
+    if ndim <= 0:
+        raise ValueError("Expected positive value for `ndim`.")
+
+    if isinstance(depth, numbers.Number):
+        depth = ndim * (depth,)
+    if not isinstance(depth, collections.Sized):
+        raise TypeError("Unexpected type for `depth`.")
+    if len(depth) != ndim:
+        raise ValueError("Expected `depth` to have a length equal to `ndim`.")
+    if isinstance(depth, collections.Sequence):
+        depth = dict(izip(irange(ndim), depth))
+    if not isinstance(depth, collections.Mapping):
+        raise TypeError("Unexpected type for `depth`.")
+
+    if not all(map(lambda d: isinstance(d, numbers.Integral), depth.values())):
+        raise TypeError("Expected integer values for `depth`.")
+    if not all(map(lambda d: d >= 0, depth.values())):
+        raise ValueError("Expected positive semidefinite values for `depth`.")
+
+    if (boundary is None) or isinstance(boundary, strlike):
+        boundary = ndim * (boundary,)
+    if not isinstance(boundary, collections.Sized):
+        raise TypeError("Unexpected type for `boundary`.")
+    if len(boundary) != ndim:
+        raise ValueError(
+            "Expected `boundary` to have a length equal to `ndim`."
+        )
+    if isinstance(boundary, collections.Sequence):
+        boundary = dict(izip(irange(ndim), boundary))
+    if not isinstance(boundary, collections.Mapping):
+        raise TypeError("Unexpected type for `boundary`.")
+
+    type_check = lambda b: (b is None) or isinstance(b, strlike)
+    if not all(map(type_check, boundary.values())):
+        raise TypeError("Expected string-like values for `boundary`.")
+
+    # Workaround for a bug in Dask with 0 depth.
+    #
+    # ref: https://github.com/dask/dask/issues/2258
+    #
+    for i in irange(ndim):
+        if boundary[i] == "none" and depth[i] == 0:
+            boundary[i] = "reflect"
+
+    return depth, boundary
