ee26a50f0e4ab69295dd652854c862c0b5655be5
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-01 16:40:42 -0400

    Add a test for _get_docstring

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -3,5 +3,30 @@
 
 from __future__ import absolute_import
 
+import inspect
 
 from dask_ndfilters import _utils
+
+
+def test__get_docstring():
+    f = lambda : 0
+
+    result = _utils._get_docstring(f)
+
+    expected = """
+    Wrapped copy of "{mod_name}.{func_name}"
+
+
+    Excludes the output parameter as it would not work Dask arrays.
+
+
+    Original docstring:
+
+    {doc}
+    """.format(
+        mod_name=inspect.getmodule(f).__name__,
+        func_name=f.__name__,
+        doc="",
+    )
+
+    assert result == expected
