4360a78c9dc4978a0b949a5ced201204c1777e65
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-02 22:15:11 -0400

    Add some tests for the uniform filter

--- tests/test_smooth.py
+++ tests/test_smooth.py
@@ -32,6 +32,25 @@ def test_gaussian_filter_params(err_type, sigma, truncate):
 
 
 @pytest.mark.parametrize(
+    "err_type, size, origin",
+    [
+        (TypeError, 3.0, 0),
+        (TypeError, 3, 0.0),
+        (RuntimeError, [3], 0),
+        (RuntimeError, 3, [0]),
+        (RuntimeError, [[3]], 0),
+        (RuntimeError, 3, [[0]]),
+    ]
+)
+def test_uniform_filter_params(err_type, size, origin):
+    a = np.arange(140.0).reshape(10, 14)
+    d = da.from_array(a, chunks=(5, 7))
+
+    with pytest.raises(err_type):
+        da_ndf.uniform_filter(d, size, origin=origin)
+
+
+@pytest.mark.parametrize(
     "sigma, truncate",
     [
         (0.0, 0.0),
@@ -65,6 +84,26 @@ def test_gaussian_identity(order, sigma, truncate):
 
 
 @pytest.mark.parametrize(
+    "size, origin",
+    [
+        (1, 0),
+    ]
+)
+def test_uniform_identity(size, origin):
+    a = np.arange(140.0).reshape(10, 14)
+    d = da.from_array(a, chunks=(5, 7))
+
+    dau.assert_eq(
+        d, da_ndf.uniform_filter(d, size, origin=origin)
+    )
+
+    dau.assert_eq(
+        sp_ndf.uniform_filter(a, size, origin=origin),
+        da_ndf.uniform_filter(d, size, origin=origin)
+    )
+
+
+@pytest.mark.parametrize(
     "sigma, truncate",
     [
         (1.0, 2.0),
@@ -92,4 +131,26 @@ def test_gaussian_compare(order, sigma, truncate):
     dau.assert_eq(
         sp_ndf.gaussian_filter(a, sigma, order, truncate=truncate),
         da_ndf.gaussian_filter(d, sigma, order, truncate=truncate)
+    )
+
+
+@pytest.mark.parametrize(
+    "size, origin",
+    [
+        (2, 0),
+        (3, 0),
+        (3, 1),
+        (3, (1, 0)),
+        ((1, 2), 0),
+        ((3, 2), (1, 0)),
+    ]
+)
+def test_uniform_compare(size, origin):
+    s = (100, 110)
+    a = np.arange(float(np.prod(s))).reshape(s)
+    d = da.from_array(a, chunks=(50, 55))
+
+    dau.assert_eq(
+        sp_ndf.uniform_filter(a, size, origin=origin),
+        da_ndf.uniform_filter(d, size, origin=origin)
     )
