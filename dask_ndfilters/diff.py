e5041a72217116cf2bc61e402570d5a3f11293ce
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-03 16:55:39 -0400

    Add wrapping of the laplace operator
    Provide a Dask interface for wrapping and computing an N-D Laplace
    operator.

--- dask_ndfilters/diff.py
+++ dask_ndfilters/diff.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+
+
+import numbers
+
+import scipy.ndimage.filters
+
+import dask_ndfilters._utils as _utils
+
+
+@_utils._update_wrapper(scipy.ndimage.filters.laplace)
+def laplace(input, mode='reflect', cval=0.0):
+    result = input.map_overlap(
+        scipy.ndimage.filters.laplace,
+        depth=(input.ndim * (1,)),
+        boundary="none",
+        dtype=input.dtype,
+        name=scipy.ndimage.filters.laplace.__name__,
+        mode=mode,
+        cval=cval
+    )
+
+    return result
