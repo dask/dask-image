695e0438e1caaaf49c6b448ac7dfdf20c2c76552
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-03 10:33:36 -0400

    Add some tests for the prewitt filter

--- tests/test_edge.py
+++ tests/test_edge.py
@@ -3,5 +3,50 @@
 
 from __future__ import absolute_import
 
+import pytest
 
-from dask_ndfilters import edge
+import numpy as np
+import scipy.ndimage.filters as sp_ndf
+
+import dask.array as da
+import dask.array.utils as dau
+
+import dask_ndfilters as da_ndf
+
+
+@pytest.mark.parametrize(
+    "err_type, axis",
+    [
+        (ValueError, 0.0),
+        (ValueError, 2),
+        (ValueError, -3),
+    ]
+)
+def test_prewitt_params(err_type, axis):
+    a = np.arange(140.0).reshape(10, 14)
+    d = da.from_array(a, chunks=(5, 7))
+
+    with pytest.raises(err_type):
+        da_ndf.prewitt(d, axis)
+
+
+@pytest.mark.parametrize(
+    "axis",
+    [
+        0,
+        1,
+        2,
+        -1,
+        -2,
+        -3,
+    ]
+)
+def test_prewitt_compare(axis):
+    s = (10, 11, 12)
+    a = np.arange(float(np.prod(s))).reshape(s)
+    d = da.from_array(a, chunks=(5, 5, 6))
+
+    dau.assert_eq(
+        sp_ndf.prewitt(a, axis),
+        da_ndf.prewitt(d, axis)
+    )
