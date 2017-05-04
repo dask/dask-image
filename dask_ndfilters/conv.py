1a6cd8a57883606a376b063cb48d84d34aed5d76
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-04 10:38:50 -0400

    Add wrapping of convolve
    Provide a Dask interface for wrapping and computing an N-D convolve
    function.

--- dask_ndfilters/conv.py
+++ dask_ndfilters/conv.py
@@ -1,1 +1,31 @@
 # -*- coding: utf-8 -*-
+
+
+import scipy.ndimage.filters
+
+import dask_ndfilters._utils as _utils
+
+
+@_utils._update_wrapper(scipy.ndimage.filters.convolve)
+def convolve(input,
+             weights,
+             mode='reflect',
+             cval=0.0,
+             origin=0):
+    origin = _utils._get_origin(weights.shape, origin)
+    depth = _utils._get_depth(weights.shape, origin)
+    depth, boundary = _utils._get_depth_boundary(input.ndim, depth, "none")
+
+    result = input.map_overlap(
+        scipy.ndimage.filters.convolve,
+        depth=depth,
+        boundary=boundary,
+        dtype=input.dtype,
+        name=scipy.ndimage.filters.convolve.__name__,
+        weights=weights,
+        mode=mode,
+        cval=cval,
+        origin=origin
+    )
+
+    return result
