c13c895eb7b80f74cd335d33d1d8f8f5debe1a81
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-04 14:25:00 -0400

    Add wrapping of correlate
    Provide a Dask interface for wrapping and computing an N-D correlate
    function

--- dask_ndfilters/conv.py
+++ dask_ndfilters/conv.py
@@ -29,3 +29,28 @@ def convolve(input,
     )
 
     return result
+
+
+@_utils._update_wrapper(scipy.ndimage.filters.correlate)
+def correlate(input,
+              weights,
+              mode='reflect',
+              cval=0.0,
+              origin=0):
+    origin = _utils._get_origin(weights.shape, origin)
+    depth = _utils._get_depth(weights.shape, origin)
+    depth, boundary = _utils._get_depth_boundary(input.ndim, depth, "none")
+
+    result = input.map_overlap(
+        scipy.ndimage.filters.correlate,
+        depth=depth,
+        boundary=boundary,
+        dtype=input.dtype,
+        name=scipy.ndimage.filters.correlate.__name__,
+        weights=weights,
+        mode=mode,
+        cval=cval,
+        origin=origin
+    )
+
+    return result
