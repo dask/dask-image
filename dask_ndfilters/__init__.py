f6cae4a3a47b2f806b48ef4696831458742bd1c9
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-05 10:48:37 -0400

    Monkey patch functions to change module name
    Modifies all functions imported in the toplevel to make them appear as
    if they were defined there.

--- dask_ndfilters/__init__.py
+++ dask_ndfilters/__init__.py
@@ -12,25 +12,38 @@ from dask_ndfilters._conv import (
     convolve,
     correlate,
 )
+convolve.__module__ = __name__
+correlate.__module__ = __name__
 
 from dask_ndfilters._diff import (
     laplace,
 )
+laplace.__module__ = __name__
+
 
 from dask_ndfilters._edge import (
     prewitt,
     sobel,
 )
+prewitt.__module__ = __name__
+sobel.__module__ = __name__
+
 
 from dask_ndfilters._gaussian import (
     gaussian_filter,
     gaussian_gradient_magnitude,
     gaussian_laplace,
 )
+gaussian_filter.__module__ = __name__
+gaussian_gradient_magnitude.__module__ = __name__
+gaussian_laplace.__module__ = __name__
+
 
 from dask_ndfilters._generic import (
     generic_filter,
 )
+generic_filter.__module__ = __name__
+
 
 from dask_ndfilters._order import (
     minimum_filter,
@@ -39,7 +52,14 @@ from dask_ndfilters._order import (
     rank_filter,
     percentile_filter,
 )
+minimum_filter.__module__ = __name__
+median_filter.__module__ = __name__
+maximum_filter.__module__ = __name__
+rank_filter.__module__ = __name__
+percentile_filter.__module__ = __name__
+
 
 from dask_ndfilters._smooth import (
     uniform_filter,
 )
+uniform_filter.__module__ = __name__
