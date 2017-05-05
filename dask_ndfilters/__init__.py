bef43fba969bdff54e94446b9afc484e880efea8
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-05 10:47:46 -0400

    Consolidate contents from core
    Move contents from core into the __init__ module for the package.

--- dask_ndfilters/__init__.py
+++ dask_ndfilters/__init__.py
@@ -7,4 +7,39 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from dask_ndfilters._core import *
+
+from dask_ndfilters._conv import (
+    convolve,
+    correlate,
+)
+
+from dask_ndfilters._diff import (
+    laplace,
+)
+
+from dask_ndfilters._edge import (
+    prewitt,
+    sobel,
+)
+
+from dask_ndfilters._gaussian import (
+    gaussian_filter,
+    gaussian_gradient_magnitude,
+    gaussian_laplace,
+)
+
+from dask_ndfilters._generic import (
+    generic_filter,
+)
+
+from dask_ndfilters._order import (
+    minimum_filter,
+    median_filter,
+    maximum_filter,
+    rank_filter,
+    percentile_filter,
+)
+
+from dask_ndfilters._smooth import (
+    uniform_filter,
+)
