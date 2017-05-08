3419ae08f1c51becf3682be78b5c406f8dfa232e
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-08 18:06:22 -0400

    Implement fourier_shift with Dask Array
    Provides an implementation of `fourier_shift` like the function found in
    SciPy's ndimage. Only handles the complex FFT case and raises an error
    if the user attempts to use the real FFT case. Includes some tests
    comparing the results to SciPy's `fourier_shift` as well.

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,9 +5,88 @@ from __future__ import absolute_import
 
 import pytest
 
+import numpy as np
+import scipy.ndimage.fourier as sp_ndf
+
+import dask.array as da
+import dask.array.utils as dau
+import dask_ndfourier as da_ndf
+
 
 def test_import_core():
     try:
         from dask_ndfourier import core
     except ImportError:
         pytest.fail("Unable to import `core`.")
+
+
+@pytest.mark.parametrize(
+    "err_type, shift, n",
+    [
+        (NotImplementedError, 0.0, 0),
+        (TypeError, 0.0 + 0.0j, 0),
+        (TypeError, {}, 0),
+        (RuntimeError, [0.0], 0),
+        (TypeError, [0.0, 0.0 + 0.0j], 0),
+        (NotImplementedError, 0, 0),
+    ]
+)
+def test_fourier_shift_err(err_type, shift, n):
+    a = np.arange(140.0).reshape(10, 14).astype(complex)
+    d = da.from_array(a, chunks=(5, 7))
+
+    with pytest.raises(err_type):
+        da_ndf.fourier_shift(d, shift, n)
+
+
+@pytest.mark.parametrize(
+    "shift",
+    [
+        0,
+        (0, 0),
+    ]
+)
+def test_fourier_shift_identity(shift):
+    a = np.arange(140.0).reshape(10, 14).astype(complex)
+    d = da.from_array(a, chunks=(5, 7))
+
+    dau.assert_eq(d, da_ndf.fourier_shift(d, shift))
+
+    dau.assert_eq(
+        sp_ndf.fourier_shift(a, shift), da_ndf.fourier_shift(d, shift)
+    )
+
+
+def test_fourier_shift_real():
+    shift = 0
+
+    a = np.arange(140.0).reshape(10, 14)
+    d = da.from_array(a, chunks=(5, 7))
+
+    dau.assert_eq(d.astype(complex), da_ndf.fourier_shift(d, shift))
+
+    dau.assert_eq(
+        sp_ndf.fourier_shift(a, shift), da_ndf.fourier_shift(d, shift)
+    )
+
+
+@pytest.mark.parametrize(
+    "shift",
+    [
+        1,
+        -1,
+        (1, 1),
+        (-1, -1),
+        (1, 0),
+        (0, 2),
+        (-1, 2),
+        (10, -9),
+    ]
+)
+def test_fourier_shift_identity(shift):
+    a = np.arange(140.0).reshape(10, 14).astype(complex)
+    d = da.from_array(a, chunks=(5, 7))
+
+    dau.assert_eq(
+        sp_ndf.fourier_shift(a, shift), da_ndf.fourier_shift(d, shift)
+    )
