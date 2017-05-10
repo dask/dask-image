3178896a266407820dc42459a8d618cc79462af8
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-05-10 10:57:46 -0400

    Add tests for vendored fftfreq
    Borrows the tests we have contributed upstream to Dask for making sure
    the vendored fftfreq works correctly. As these are our contribution, we
    are ok to include them here as is.

--- tests/test__compat.py
+++ tests/test__compat.py
@@ -8,6 +8,7 @@ import pytest
 import numpy as np
 
 import dask
+import dask.array as da
 import dask.array.utils as dau
 
 import dask_ndfourier._compat
@@ -69,3 +70,12 @@ def test_indicies():
     darr = dask_ndfourier._compat._indices((2, 3), chunks=(1, 2))
     nparr = np.indices((2, 3))
     dau.assert_eq(darr, nparr)
+
+
+@pytest.mark.parametrize("n", [1, 2, 3, 6, 7])
+@pytest.mark.parametrize("d", [1.0, 0.5, 2 * np.pi])
+def test_fftfreq(n, d):
+    dau.assert_eq(
+        dask_ndfourier._compat._fftfreq(n, d, chunks=((n,),)),
+        np.fft.fftfreq(n, d)
+    )
