38c8484634e43636425f85b4f1485255f614971b
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-06 20:39:10 -0400

    Test error handling in imread
    Add some tests to make sure proper errors are raised when improper
    inputs are provided. Namely an unacceptable number of frames to use for
    chunking.

--- tests/test_dask_imread.py
+++ tests/test_dask_imread.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from __future__ import absolute_import
 
 import pytest
 
+import dask_imread
 
-def test_import_toplevel():
-    try:
-        import dask_imread
-    except ImportError:
-        pytest.fail("Unable to import `dask_imread`.")
+
+@pytest.mark.parametrize(
+    "err_type, nframes",
+    [
+        (ValueError, 1.0),
+        (ValueError, 0),
+        (ValueError, -1),
+    ]
+)
+def test_errs_imread(err_type, nframes):
+    with pytest.raises(err_type):
+        dask_imread.imread("test.tiff", nframes=nframes)
