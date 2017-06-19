8e59577457e4646649fa53715d0f5815c97b6fd4
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-19 19:25:22 -0400

    Backport fftfreq test change for handling chunks
    This is a backported change that we submitted to Dask to test different
    kinds of chunks that could be specified to `fftfreq`. The change to
    `fftfreq` has already been incorporated. So this just exercise it a bit
    more.
    
    ref: https://github.com/dask/dask/commit/6a7a507258940b9dd52adfde5b1692bf178b5f41

--- tests/test__compat.py
+++ tests/test__compat.py
@@ -16,8 +16,10 @@ import dask_ndfourier._compat
 
 @pytest.mark.parametrize("n", [1, 2, 3, 6, 7])
 @pytest.mark.parametrize("d", [1.0, 0.5, 2 * np.pi])
-def test_fftfreq(n, d):
+@pytest.mark.parametrize("c", [lambda m: m, lambda m: (1, m - 1)])
+def test_fftfreq(n, d, c):
+    c = c(n)
     dau.assert_eq(
-        dask_ndfourier._compat._fftfreq(n, d, chunks=((n,),)),
+        dask_ndfourier._compat._fftfreq(n, d, chunks=c),
         np.fft.fftfreq(n, d)
     )
