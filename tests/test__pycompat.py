be5eecf2a043abf7585022f1dda3b79572eba192
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-29 19:19:14 -0400

    Add a basic test for irange
    Make sure `irange` is there, it doesn't return a list, and it acts like
    `range` on some test arguments.

--- tests/test__pycompat.py
+++ tests/test__pycompat.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+from __future__ import absolute_import
+
+import dask_ndmeasure._pycompat
+
+
+def test_irange():
+    r = dask_ndmeasure._pycompat.irange(5)
+
+    assert not isinstance(r, list)
+
+    assert list(r) == [0, 1, 2, 3, 4]
