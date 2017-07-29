39ddd37d4a8a45af3a0827133d80fea0635738ed
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-29 18:39:47 -0400

    Tests the label matching function
    Provides a parameterized test to exercise the comparison of selected
    indices to the label image and the application of these masks to the
    input data. Tries different formulation of indices. Includes a NumPy
    variant that it compares the results of to our Dask Array
    implementation.

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import
 
+import operator
+
 import pytest
 
 import numpy as np
@@ -53,3 +55,54 @@ def test__norm_input_labels_index():
     dau.assert_eq(d_n, d)
     dau.assert_eq(d_lbls_n, d_lbls)
     dau.assert_eq(ind_n, np.array([1], dtype=np.int64))
+
+
+@pytest.mark.parametrize(
+    "shape, chunks, ind", [
+        ((15, 16), (4, 5), 0),
+        ((15, 16), (4, 5), 1),
+        ((15, 16), (4, 5), [1]),
+        ((15, 16), (4, 5), [1, 2]),
+        ((15, 16), (4, 5), [1, 100]),
+        ((15, 16), (4, 5), [[1, 2, 3, 4]]),
+        ((15, 16), (4, 5), [[1, 2], [3, 4]]),
+        ((15, 16), (4, 5), [[[1], [2], [3], [4]]]),
+    ]
+)
+def test__get_label_matches(shape, chunks, ind):
+    a = np.random.random(shape)
+    d = da.from_array(a, chunks=chunks)
+
+    lbls = np.zeros(a.shape, dtype=np.int64)
+    lbls += (
+        (a < 0.5).astype(lbls.dtype) +
+        (a < 0.25).astype(lbls.dtype) +
+        (a < 0.125).astype(lbls.dtype) +
+        (a < 0.0625).astype(lbls.dtype)
+    )
+    d_lbls = da.from_array(lbls, chunks=d.chunks)
+
+    ind = np.array(ind)
+    d_ind = da.from_array(ind, chunks=1)
+
+    lbl_mtch = operator.eq(
+        ind[(Ellipsis,) + lbls.ndim * (None,)],
+        lbls[ind.ndim * (None,)]
+    )
+    input_i_mtch = (
+        lbl_mtch.astype(np.int64)[ind.ndim * (slice(None),) + (None,)] *
+        np.indices(a.shape, dtype=np.int64)[ind.ndim * (None,)]
+    )
+    input_mtch = lbl_mtch.astype(a.dtype) * a[ind.ndim * (None,)]
+
+    d_lbl_mtch, d_input_i_mtch, d_input_mtch = dask_ndmeasure._utils._get_label_matches(
+        d, d_lbls, ind
+    )
+
+    assert issubclass(d_lbl_mtch.dtype.type, np.bool8)
+    assert issubclass(d_input_i_mtch.dtype.type, np.int64)
+    assert issubclass(d_input_mtch.dtype.type, a.dtype.type)
+
+    dau.assert_eq(d_lbl_mtch, lbl_mtch)
+    dau.assert_eq(d_input_i_mtch, input_i_mtch)
+    dau.assert_eq(d_input_mtch, input_mtch)
