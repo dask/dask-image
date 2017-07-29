040c848308fee0eb113575d1aa4b6ff3247f2efc
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-29 18:26:41 -0400

    Adds a function to get label matches
    Provides a simple refactored internal utility function that uses the
    input array, label array, and selected indices to find masks of selected
    labels in order and use the masks to selected relevant regions in the
    input data. Both the masks and selected input data are returned to the
    caller.

--- dask_ndmeasure/_utils.py
+++ dask_ndmeasure/_utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+import operator
+
 import numpy
 
 import dask.array
@@ -35,3 +37,26 @@ def _norm_input_labels_index(input, labels=None, index=None):
         raise ValueError("The input and labels arrays must be the same shape.")
 
     return (input, labels, index)
+
+
+def _get_label_matches(input, labels, index):
+    input_i = _compat._indices(
+        input.shape, dtype=numpy.int64, chunks=input.chunks
+    )
+
+    lbl_mtch = operator.eq(
+        index[(Ellipsis,) + labels.ndim * (None,)],
+        labels[index.ndim * (None,)]
+    )
+
+    input_i_mtch = dask.array.where(
+        lbl_mtch[index.ndim * (slice(None),) + (None,)],
+        input_i[index.ndim * (None,)],
+        input_i.dtype.type(0)
+    )
+
+    input_mtch = dask.array.where(
+        lbl_mtch, input[index.ndim * (None,)], input.dtype.type(0)
+    )
+
+    return (lbl_mtch, input_i_mtch, input_mtch)
