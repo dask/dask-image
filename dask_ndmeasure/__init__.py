faa53e0e905ec86a00fa4c146293869148a644c2
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-29 16:43:47 -0400

    Use arg normalizing function in center_of_mass
    As this normalization code has been refactored out into an independent
    internal utility function, simply make use of it to implement
    `center_of_mass`.

--- dask_ndmeasure/__init__.py
+++ dask_ndmeasure/__init__.py
@@ -18,6 +18,7 @@ import numpy
 import dask.array
 
 from . import _compat
+from . import _utils
 
 
 def center_of_mass(input, labels=None, index=None):
@@ -42,26 +43,9 @@ def center_of_mass(input, labels=None, index=None):
         Coordinates of centers-of-mass.
     """
 
-    input = _compat._asarray(input)
-
-    if labels is None:
-        labels = (input != 0).astype(numpy.int64)
-        index = None
-
-    if index is None:
-        labels = (labels > 0).astype(numpy.int64)
-        index = dask.array.ones(tuple(), dtype=numpy.int64, chunks=tuple())
-
-    labels = _compat._asarray(labels)
-    index = _compat._asarray(index)
-
-    # SciPy transposes these for some reason.
-    # So we do the same thing here.
-    # This only matters if index is some array.
-    index = index.T
-
-    if input.shape != labels.shape:
-        raise ValueError("The input and labels arrays must be the same shape.")
+    input, labels, index = _utils._norm_input_labels_index(
+        input, labels, index
+    )
 
     input_i = _compat._indices(input.shape, chunks=input.chunks)
 
