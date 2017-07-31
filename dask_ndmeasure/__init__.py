e35a6d2512dca300510768d129850118b2f2d52b
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-31 00:18:08 -0400

    Rewrite center_of_mass using sum
    As the behavior in `center_of_mass` can basically be seen as computing a
    few arrays and summing them, just simplify the summing portions by
    reusing our `sum` function for them. This significantly reduces the
    complexity of the `center_of_mass` function.

--- dask_ndmeasure/__init__.py
+++ dask_ndmeasure/__init__.py
@@ -52,33 +52,20 @@ def center_of_mass(input, labels=None, index=None):
     # This only matters if index is some array.
     index = index.T
 
+    input_mtch_sum = sum(input, labels, index)
+
     input_i = _compat._indices(
         input.shape, dtype=numpy.int64, chunks=input.chunks
     )
+
     input_i_wt = input[None] * input_i
 
-    lbl_mtch = _utils._get_label_matches(labels, index)
+    input_i_wt_mtch_sum = []
+    for i in _pycompat.irange(len(input_i_wt)):
+        input_i_wt_mtch_sum.append(sum(input_i_wt[i], labels, index))
+    input_i_wt_mtch_sum = dask.array.stack(input_i_wt_mtch_sum, axis=-1)
 
-    input_i_wt_mtch = dask.array.where(
-        lbl_mtch[index.ndim * (slice(None),) + (None,)],
-        input_i_wt[index.ndim * (None,)],
-        input_i_wt.dtype.type(0)
-    )
-
-    input_mtch = dask.array.where(
-        lbl_mtch, input[index.ndim * (None,)], input.dtype.type(0)
-    )
-
-    input_i_wt_mtch = input_i_wt_mtch.astype(numpy.float64)
-    input_mtch = input_mtch.astype(numpy.float64)
-
-    com_lbl = input_i_wt_mtch.sum(
-        axis=tuple(_pycompat.irange(1 + index.ndim, input_i_wt_mtch.ndim))
-    )
-    input_mtch_sum = input_mtch.sum(
-        axis=tuple(_pycompat.irange(index.ndim, input_mtch.ndim))
-    )
-    com_lbl /= input_mtch_sum[index.ndim * (slice(None),) + (None,)]
+    com_lbl = input_i_wt_mtch_sum / input_mtch_sum[..., None]
 
     return com_lbl
 
