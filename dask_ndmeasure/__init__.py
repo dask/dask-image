ecd897f15b8df0f4be7aca799d6ca2ee136d1a93
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-08-02 00:33:59 -0400

    Rewrite of maximum that uses nanmax
    This basically is a clever trick to avoid using `labeled_comprehension`
    in the computation of `maximum`. It works by converting the data to
    `float64` and placing `nan` over all unneeded values. This way `nanmax`
    can be run and all of the `nan` values will be ignored (unless there are
    only `nan` values). Using info about which labels could be computed,
    `where` is run over the final result to replace those values that could
    not be computed with `0`. The result is then cast back to its original
    type. This change up should allow the computation to proceed faster than
    it would by going through `delayed` in `labeled_comprehension`.

--- dask_ndmeasure/__init__.py
+++ dask_ndmeasure/__init__.py
@@ -238,9 +238,21 @@ def maximum(input, labels=None, index=None):
         input, labels, index
     )
 
-    max_lbl = labeled_comprehension(
-        input, labels, index, numpy.max, input.dtype, 0
+    lbl_mtch = _utils._get_label_matches(labels, index)
+
+    lbl_mtch_any = lbl_mtch.any(
+        axis=tuple(_pycompat.irange(index.ndim, lbl_mtch.ndim))
     )
+
+    input_mtch = dask.array.where(
+        lbl_mtch, input[index.ndim * (None,)], numpy.nan
+    )
+
+    nanmax_lbl = dask.array.nanmax(
+        input_mtch, axis=tuple(_pycompat.irange(index.ndim, input_mtch.ndim))
+    )
+
+    max_lbl = dask.array.where(lbl_mtch_any, nanmax_lbl, 0).astype(input.dtype)
 
     return max_lbl
 
