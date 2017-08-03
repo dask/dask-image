fe452aab09b5fda157229efb39379f830dac00de
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-08-03 11:12:50 -0400

    Rewrite maximum to use labeled_comprehension
    Based on recent optimizations to `labeled_comprehension`, it appears
    that finding the maximum with `labeled_comprehension` is faster than
    trying to the original implementation of `maximum`. So simply rewrite
    `maximum` to use `labeled_comprehension` instead. This will also improve
    the test coverage of the `labeled_comprehension` code path.

--- dask_ndmeasure/__init__.py
+++ dask_ndmeasure/__init__.py
@@ -232,23 +232,9 @@ def maximum(input, labels=None, index=None):
         input, labels, index
     )
 
-    lbl_mtch = _utils._get_label_matches(labels, index)
-
-    lbl_mtch_any = lbl_mtch.any(
-        axis=tuple(_pycompat.irange(index.ndim, lbl_mtch.ndim))
+    return labeled_comprehension(
+        input, labels, index, numpy.max, input.dtype, input.dtype.type(0)
     )
-
-    input_mtch = dask.array.where(
-        lbl_mtch, input[index.ndim * (None,)], numpy.nan
-    )
-
-    nanmax_lbl = dask.array.nanmax(
-        input_mtch, axis=tuple(_pycompat.irange(index.ndim, input_mtch.ndim))
-    )
-
-    max_lbl = dask.array.where(lbl_mtch_any, nanmax_lbl, 0).astype(input.dtype)
-
-    return max_lbl
 
 
 def maximum_position(input, labels=None, index=None):
