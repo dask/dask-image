103e5782ddb436f084eca16b4d7013d0d15b25a8
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-08-07 16:40:22 -0400

    Drop some unneeded code from utils tests
    This code was used to compare results of the `_get_label_matches`
    function. However that function no longer returns these other values. So
    there is no need to generate something similar in the tests as the
    comparison is not being made.

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -126,12 +126,6 @@ def test__get_label_matches(shape, chunks, ind):
         ind[(Ellipsis,) + lbls.ndim * (None,)],
         lbls[ind.ndim * (None,)]
     )
-    input_i_mtch = (
-        lbl_mtch.astype(np.int64)[ind.ndim * (slice(None),) + (None,)] *
-        np.indices(a.shape, dtype=np.int64)[ind.ndim * (None,)]
-    )
-    input_mtch = lbl_mtch.astype(a.dtype) * a[ind.ndim * (None,)]
-
     d_lbl_mtch = dask_ndmeasure._utils._get_label_matches(d_lbls, d_ind)
 
     assert issubclass(d_lbl_mtch.dtype.type, np.bool8)
