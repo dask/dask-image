26f4bdfb12fc0f1f91fa6b439903f785165a0d1a
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-02-06 15:46:28 +1100

    Revert "Bump Dask requirement to 0.18.2"
    This reverts commit 2b2a84e21735ae1e53c4c24d5756b94f7c3f534c.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "dask[array] >=0.18.2",
+    "dask[array] >=0.16.1",
     "numpy >=1.11.3",
     "scipy >=0.19.1",
     "pims >=0.4.1",
