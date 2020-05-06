a9daab5e7e8b11e7035f2ef7feae791f5857867c
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-05-07 09:05:48 +1000

    Support Python 3.8 with CI tests (and drop python 3.5) (#131)
    * Appveyor python3.8 test environment
    
    * CircleCI python3.8 test environment
    
    * Travis python3.8 test environment
    
    * Mention python 3.8 in the pull request template
    
    * Add python 3.8 to setup.py tags
    
    * Add empty .coveralls.yml
    
    * Garbage commit needed to re-trigger CircleCI builds again
    
    * Add content to .coveralls.yml
    
    * Drop support for python 3.5
    
    * Remove python 3.5 jobs from continuous integration
    
    * Remove Travis python 3.5 job
    
    * Update CircleCI build jobs list
    
    * Update miniconda to 4.8.2 for CI
    
    * Update Appveyor miniconda to 4.8.2
    
    * Update MD5 hash for miniconda
    
    * Update Travis miniconda to 4.8.2
    
    * Update CircleCI miniconda to 4.8.2

--- .circleci/install_miniconda.sh
+++ .circleci/install_miniconda.sh
@@ -4,11 +4,11 @@
 set -xeuo pipefail
 
 # Miniconda Config:
-export MINICONDA_VERSION="4.5.4"
-export MINICONDA_MD5="a946ea1d0c4a642ddf0c3a26a18bb16d"
+export MINICONDA_VERSION="4.8.2"
+export MINICONDA_MD5="87e77f097f6ebb5127c77662dfc3165e"
 export MINICONDA_INSTALLER="${HOME}/miniconda.sh"
 export MINICONDA_DIR="${HOME}/miniconda"
-export MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh"
+export MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-py37_${MINICONDA_VERSION}-Linux-x86_64.sh"
 
 # Install Miniconda.
 curl -L "${MINICONDA_URL}" > "${MINICONDA_INSTALLER}"
