6f9df7309e69d57be1e3e6b8644f34bec2a6abe1
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-06-23 19:20:46 -0400

    move test to test folder and add __init__

---
+++ dask_image/feature/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .template import match_template
+
+match_template.__module__ = __name__
---
+++ tests/test_dask_image/test_feature/test_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import dask.array as da
+
+from skimage import data, color, morphology, feature
+
+import dask_image.feature as da_feat
+
+
+def test_match_template():
+    img = data.astronaut()
+    img = color.rgb2gray(img)
+    img = da.from_array(img)
+    p = dict(image=img, template=morphology.square(3), mode='constant',
+             pad_input=True)
+
+    response_sk = feature.match_template(**p)
+    response_da = da_feat.match_template(**p).compute()
+
+    assert np.array_equal(response_sk, response_da)
