3c4b7a0ad0a2d2cb67027e5025814d2c08dfa290
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-17 10:37:57 +1000

    Remove commented trial & error

--- dask_image/utils/_dispatcher.py
+++ dask_image/utils/_dispatcher.py
@@ -39,24 +39,6 @@ def numpy_convolve(*args, **kwargs):
     return scipy.ndimage.filters.convolve(*args, **kwargs)
 
 
-# import cupy
-# import cupyx.scipy.ndimage
-
-# @dispatch_convolve.register(cupy.core.core.ndarray)
-# def cupy_convolve(*args, **kwargs):
-#     return cupyx.scipy.ndimage.filters.convolve(*args, **kwargs)
-
-
-# @dispatch_convolve.register_lazy("numpy")
-# def register_numpy():
-#     import numpy as np
-#     import scipy.ndimage
-
-#     @dispatch_convolve.register(np.ndarray)
-#     def numpy_convolve(*args, **kwargs):
-#         return scipy.ndimage.filters.convolve(*args, **kwargs)
-
-
 @dispatch_convolve.register_lazy("cupy")
 def register_cupy():
     import cupy
