e385895b9cb74bae3d1a71ef96602b1b2b21f0d0
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-17 11:01:13 +1000

    Add docstrings for dispatch helper functions

--- dask_image/utils/_dispatcher.py
+++ dask_image/utils/_dispatcher.py
@@ -4,15 +4,25 @@ from dask.utils import Dispatch
 
 
 def check_arraytypes_compatible(*args):
-    """"""
+    """Check array types are compatible.
+
+    For arrays to be compatible they must either have the same type,
+    or a dask array where the chunks match the same array type.
+
+    Examples of compatible arrays:
+    * Two (or more) numpy arrays
+    * A dask array with numpy chunks, and a numpy array
+
+    Examples of incompatible arrays:
+    * A numpy array and a cupy array
+    """
     arraytypes = [get_type(arg) for arg in args]
     if len(set(arraytypes)) != 1:
         raise ValueError("Array types must be compatible.")
 
 
-
 def get_type(arg):
-    """"""
+    """Return type of arrays contained within the dask array chunks."""
     try:
         datatype = type(arg._meta)  # Check chunk type backing dask array
     except AttributeError:
@@ -33,6 +43,7 @@ class Dispatcher(Dispatch):
 
 
 dispatch_convolve = Dispatcher(name="dispatch_convolve")
+
 
 @dispatch_convolve.register(np.ndarray)
 def numpy_convolve(*args, **kwargs):
