ad58e45cf6d59f2545e067f9613c280a3d5d8aa7
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-17 10:36:13 +1000

    Try being more specific about the cupy array type

--- dask_image/utils/_dispatcher.py
+++ dask_image/utils/_dispatcher.py
@@ -43,8 +43,9 @@ def numpy_convolve(*args, **kwargs):
 @dispatch_convolve.register_lazy("cupy")
 def register_cupy():
     import cupy
+    import cupy.core.core
     import cupyx.scipy.ndimage
 
-    @dispatch_convolve.register(cupy.ndarray)
+    @dispatch_convolve.register(cupy.core.core.ndarray)
     def cupy_convolve(*args, **kwargs):
         return cupyx.scipy.ndimage.filters.convolve(*args, **kwargs)
