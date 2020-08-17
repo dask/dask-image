bf5cd49762a62217b76ddabad79d57471d08e9dc
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-17 17:29:17 +1000

    Dispatch edits (#7)
    * Remove reference to closed dask issue 6442
    
    * John's suggestions for streamlining dispatch
    
    * Use meta kwarg to fix array chunktype repr
    
    * This shouldn't make a difference, but checking just in case
    
    * Fix up dispatch lazy registration, and output array metadata
    
    * Non-lazy cupy dispatcher
    
    * Lazy cupy dispatching again
    
    * Propagate array type metadata through map_overlap
    
    * Try being more specific about the cupy array type
    
    * Remove commented trial & error
    
    * flake8 - remove unused import
    
    * Add docstrings for dispatch helper functions
    
    * Bump dask minimum version to 2.8.1 in CircleCI
    
    * Bump minimum dask version to 2.8.1 for all CI services

--- dask_image/ndfilters/_conv.py
+++ dask_image/ndfilters/_conv.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import scipy.ndimage.filters
 
 from . import _utils
-from ..utils._dispatcher import convolve_dispatch
+from ..utils._dispatcher import dispatch_convolve, check_arraytypes_compatible
 
 
 @_utils._update_wrapper(scipy.ndimage.filters.convolve)
@@ -12,15 +11,18 @@ def convolve(image,
              mode='reflect',
              cval=0.0,
              origin=0):
+    check_arraytypes_compatible(image, weights)
+
     origin = _utils._get_origin(weights.shape, origin)
     depth = _utils._get_depth(weights.shape, origin)
     depth, boundary = _utils._get_depth_boundary(image.ndim, depth, "none")
 
     result = image.map_overlap(
-        convolve_dispatch(image),
+        dispatch_convolve,
         depth=depth,
         boundary=boundary,
         dtype=image.dtype,
+        meta=image._meta,
         weights=weights,
         mode=mode,
         cval=cval,
--- dask_image/utils/_dispatcher.py
+++ dask_image/utils/_dispatcher.py
@@ -3,6 +3,33 @@ import scipy.ndimage.filters
 from dask.utils import Dispatch
 
 
+def check_arraytypes_compatible(*args):
+    """Check array types are compatible.
+
+    For arrays to be compatible they must either have the same type,
+    or a dask array where the chunks match the same array type.
+
+    Examples of compatible arrays:
+    * Two (or more) numpy arrays
+    * A dask array with numpy chunks, and a numpy array
+
+    Examples of incompatible arrays:
+    * A numpy array and a cupy array
+    """
+    arraytypes = [get_type(arg) for arg in args]
+    if len(set(arraytypes)) != 1:
+        raise ValueError("Array types must be compatible.")
+
+
+def get_type(arg):
+    """Return type of arrays contained within the dask array chunks."""
+    try:
+        datatype = type(arg._meta)  # Check chunk type backing dask array
+    except AttributeError:
+        datatype = type(arg)  # For all non-dask arrays
+    return datatype
+
+
 class Dispatcher(Dispatch):
     """Simple single dispatch for different dask array types."""
 
@@ -10,23 +37,25 @@ class Dispatcher(Dispatch):
         """
         Call the corresponding method based on type of dask array.
         """
-        meth = self.dispatch(type(arg._meta))
+        datatype = get_type(arg)
+        meth = self.dispatch(datatype)
         return meth(arg, *args, **kwargs)
 
 
-convolve_dispatch = Dispatcher(name="convolve_dispatch")
+dispatch_convolve = Dispatcher(name="dispatch_convolve")
 
 
-@convolve_dispatch.register(np.ndarray)
+@dispatch_convolve.register(np.ndarray)
 def numpy_convolve(*args, **kwargs):
-    return scipy.ndimage.filters.convolve
+    return scipy.ndimage.filters.convolve(*args, **kwargs)
 
 
-@convolve_dispatch.register_lazy("cupy")
+@dispatch_convolve.register_lazy("cupy")
 def register_cupy():
     import cupy
+    import cupy.core.core
     import cupyx.scipy.ndimage
 
-    @convolve_dispatch.register(cupy.ndarray)
+    @dispatch_convolve.register(cupy.core.core.ndarray)
     def cupy_convolve(*args, **kwargs):
-        return cupyx.scipy.ndimage.filters.convolve
+        return cupyx.scipy.ndimage.filters.convolve(*args, **kwargs)
