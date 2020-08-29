c5674fb021484fcb7de27bf87b85f22cdb54cc72
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-30 01:18:20 +1000

    Add basic cupy tests for all ndfilter functions

--- tests/test_dask_image/test_ndfilters/test_cupy_ndfilters.py
+++ tests/test_dask_image/test_ndfilters/test_cupy_ndfilters.py
@@ -5,16 +5,83 @@ import dask.array as da
 import numpy as np
 import pytest
 
-import dask_image.ndfilters as da_ndf
+from dask_image import ndfilters
 
 cupy = pytest.importorskip("cupy", minversion="7.0.0")
 
 
-@pytest.mark.cupy
-def test_cupy_convolve():
-    """Test convolve filter with cupy input arrays."""
+@pytest.fixture
+def array():
     s = (10, 10)
     a = da.from_array(cupy.arange(int(np.prod(s)), dtype=cupy.float32).reshape(s), chunks=5)
-    w = cupy.ones(a.ndim * (3,), dtype=cupy.float32)
-    result = da_ndf.convolve(a, w)
+    return a
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.convolve,
+    ndfilters.correlate,
+])
+def test_cupy_conv(array, func):
+    """Test convolve & correlate filters with cupy input arrays."""
+    weights = cupy.ones(array.ndim * (3,), dtype=cupy.float32)
+    result = func(array, weights)
+    result.compute()
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.laplace,
+])
+def test_cupy_diff(array, func):
+    result = func(array)
+    result.compute()
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.prewitt,
+    ndfilters.sobel,
+])
+def test_cupy_edge(array, func):
+    result = func(array)
+    result.compute()
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.gaussian_filter,
+    ndfilters.gaussian_gradient_magnitude,
+    ndfilters.gaussian_laplace,
+])
+def test_cupy_gaussian(array, func):
+    sigma = 3
+    result = func(array, sigma)
+    result.compute()
+
+
+def test_cupy_generic(array):
+    result = ndfilters.generic_filter(array, sum)
+    result.compute()
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.minimum_filter,
+    ndfilters.median_filter,
+    ndfilters.maximum_filter,
+    ndfilters.rank_filter,
+    ndfilters.percentile_filter,
+])
+def test_cupy_order(array, func):
+    result = func(array)
+    result.compute()
+
+
+@pytest.mark.cupy
+@pytest.mark.parametrize("func", [
+    ndfilters.uniform_filter,
+])
+def test_cupy_smooth(array, func):
+    result = func(array)
     result.compute()
