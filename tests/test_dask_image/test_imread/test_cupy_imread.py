e1d4c57b22d37649f758b79f2280846c45e9d64b
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-31 00:12:52 +1000

    Add cupy tests for imread

---
+++ tests/test_dask_image/test_imread/test_cupy_imread.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tifffile
+import pytest
+
+import dask_image.imread
+
+cupy = pytest.importorskip("cupy", minversion="7.7.0")
+
+
+@pytest.mark.cupy
+def test_cupy_imread(tmp_path):
+    a = np.random.uniform(low=0.0, high=1.0, size=(1, 4, 3)).astype(np.float32)
+
+    fn = str(tmp_path.join("test.tiff"))
+    with tifffile.TiffWriter(fn) as fh:
+        for i in range(len(a)):
+            fh.save(a[i])
+
+    result = dask_image.imread.imread(fn, arraytype="cupy")
+    assert type(result._meta) == cupy.ndarray
+    assert type(result.compute()) == cupy.ndarray
