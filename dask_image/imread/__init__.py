f638d4317206a414060e94c07e15f9acbe28e021
Tetsuo Koyama <tkoyama010@gmail.com>
2021-02-18 12:58:45 +0900

    Fix error of import (imread)

--- dask_image/imread/__init__.py
+++ dask_image/imread/__init__.py
@@ -5,14 +5,4 @@ __author__ = """John Kirkham"""
 __email__ = "kirkhamj@janelia.hhmi.org"
 
 
-import itertools
-import numbers
-import warnings
-
-import dask
-import dask.array as da
-import dask.delayed
-import numpy as np
-import pims
-
 from ._utils import imread
--- dask_image/imread/_utils.py
+++ dask_image/imread/_utils.py
@@ -5,6 +5,13 @@ __author__ = """John Kirkham"""
 __email__ = "kirkhamj@janelia.hhmi.org"
 
 
+import itertools
+import numbers
+import warnings
+
+import dask
+import dask.array as da
+import dask.delayed
 import numpy as np
 import pims
 
