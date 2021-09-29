a858c61ac5beb7de7d7644d7e85714b5c16c2a7a
anlavandier <90333202+anlavandier@users.noreply.github.com>
2021-09-29 05:08:01 +0200

    Fix wrap bug in ndfilters convolve and correlate (#243)
    * Bug fixing ndfilters.convolve and ndfilter.correlate
    
    * Added tests
    
    * Fixed a syntax error
    
    * Update dask_image/ndfilters/_conv.py
    
    Co-authored-by: Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
    
    * Update dask_image/ndfilters/_conv.py
    
    Co-authored-by: Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
    
    Co-authored-by: Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
    
    Why do we set `mode=constant` here? "The reason I did it is because I assume that it is (barely) faster than doing reflect because padding with constant values is easier than padding reflectively." -  anlavandier
    Gen also assumes that it is unlikely to cause edge artifacts between chunks, because the user can adjust the map_overlap depth to the point where the scipy chunk boundary modes don't really affect the final Dask result.

--- dask_image/ndfilters/_conv.py
+++ dask_image/ndfilters/_conv.py
@@ -13,16 +13,16 @@ __all__ = [
 
 
 @_utils._update_wrapper(scipy.ndimage.filters.convolve)
-def convolve(image,
-             weights,
-             mode='reflect',
-             cval=0.0,
-             origin=0):
+def convolve(image, weights, mode="reflect", cval=0.0, origin=0):
     check_arraytypes_compatible(image, weights)
 
     origin = _utils._get_origin(weights.shape, origin)
     depth = _utils._get_depth(weights.shape, origin)
     depth, boundary = _utils._get_depth_boundary(image.ndim, depth, "none")
+
+    if mode == "wrap":  # Fixes https://github.com/dask/dask-image/issues/242
+        boundary = "periodic"
+        mode = "constant"
 
     result = image.map_overlap(
         dispatch_convolve(image),
@@ -33,23 +33,23 @@ def convolve(image,
         weights=weights,
         mode=mode,
         cval=cval,
-        origin=origin
+        origin=origin,
     )
 
     return result
 
 
 @_utils._update_wrapper(scipy.ndimage.filters.correlate)
-def correlate(image,
-              weights,
-              mode='reflect',
-              cval=0.0,
-              origin=0):
+def correlate(image, weights, mode="reflect", cval=0.0, origin=0):
     check_arraytypes_compatible(image, weights)
 
     origin = _utils._get_origin(weights.shape, origin)
     depth = _utils._get_depth(weights.shape, origin)
     depth, boundary = _utils._get_depth_boundary(image.ndim, depth, "none")
+
+    if mode == "wrap":  # Fixes https://github.com/dask/dask-image/issues/242
+        boundary = "periodic"
+        mode = "constant"
 
     result = image.map_overlap(
         dispatch_correlate(image),
@@ -60,7 +60,7 @@ def correlate(image,
         weights=weights,
         mode=mode,
         cval=cval,
-        origin=origin
+        origin=origin,
     )
 
     return result
