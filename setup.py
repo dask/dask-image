354fbd9b8ce4e7166975f6280d4efc178bcfe344
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-01-31 12:54:57 -0500

    Bump CI / docs to support python 3.9

--- setup.py
+++ setup.py
@@ -70,10 +70,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     tests_require=test_requirements
 )
