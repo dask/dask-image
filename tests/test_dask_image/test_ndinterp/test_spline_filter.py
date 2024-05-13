89eb4f113f72bb0ae2c941d9473cf61db0e0a58c
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2024-05-13 17:33:56 +1000

    pytest.skip reason needs to be an unnamed argument

--- tests/test_dask_image/test_ndinterp/test_spline_filter.py
+++ tests/test_dask_image/test_ndinterp/test_spline_filter.py
@@ -172,7 +172,7 @@ def test_spline_filter1d_general(
     axis,
 ):
     if axis == 1 and n < 2:
-        pytest.skip(msg="skip axis=1 for 1d signals")
+        pytest.skip("skip axis=1 for 1d signals")
 
     validate_spline_filter(
         n=n,
