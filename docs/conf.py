332df06b5a53a7f33facc5fbc6bae5265d115475
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2024-05-21 18:24:36 +1000

    Fix problem where RTD displays full release version string (eg: X.Y.Z.dev0+gXXXXXX)., not shorter X.Y.Z version

--- docs/conf.py
+++ docs/conf.py
@@ -130,11 +130,11 @@ html_theme = 'dask_sphinx_theme'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = f"{project} {version} documentation"
 
 # A shorter title for the navigation bar.  Default is the same as
 # html_title.
-#html_short_title = None
+html_short_title = f"{project} docs"
 
 # The name of an image file (relative to this directory) to place at the
 # top of the sidebar.
