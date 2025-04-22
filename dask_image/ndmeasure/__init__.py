7f26751378231d3c2abf10d4e185a3c058a47da1
Marvin Albert <marvin.albert@gmail.com>
2025-04-22 23:01:32 +0200

    Fix another typo

--- dask_image/ndmeasure/__init__.py
+++ dask_image/ndmeasure/__init__.py
@@ -222,7 +222,7 @@ def find_objects(label_image):
     Returns
     -------
     Dask dataframe
-        Each row represents an individual integrer label. Columns contain the
+        Each row represents an individual integer label. Columns contain the
         slice information for the object boundaries in each dimension
         (dimensions are named: 0, 1, ..., nd).
 
